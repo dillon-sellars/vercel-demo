dbf061a88db831a48f3522faf8557d57da17e358
Dillon Sellars <dill.sellars@gmail.com>
2024-01-08 06:57:07 -0500

    Wish list

---
+++ app/wish/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Page() {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-4xl">
+        Dillon&rsquo;s Wish List
+      </h1>
+      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6 md:text-2xl">
+        Make tacos for my friends.
+      </p>
+    </main>
+  );
+}
